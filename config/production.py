c27b2e61f670bc0e11fef87ef418ce2c9b1e8184
이성재 <sungjaeroro0309@naver.com>
2025-04-07 16:19:12 +0900

    sqlalchemy_database_uri 설정

--- config/production.py
+++ config/production.py
@@ -1,7 +1,14 @@
 from logging.config import dictConfig
 from config.default import *
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'7\xd2\xa4\xf4\x0f\xd2\xcc\x0bT\xce\xe7\xfd;\\3h'
 
