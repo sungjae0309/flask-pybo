9408644ff575f4fffa6b1b6b42c3de3ca10479f9
이성재 <sungjaeroro0309@naver.com>
2025-04-07 15:27:55 +0900

    add logging to ini

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
@@ -12,5 +12,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    3/0  # 강제로 오류발생
-    return redirect(url_for('question._list'))
+    current_app.logger.info("INFO 레벨로 출력")
